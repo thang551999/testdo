ef9da1b23ef5abf79d542a5ec632724bc518eebe
quanduong1999 <dangquan2909@gmail.com>
2021-08-04 16:12:15 +0700

    title

--- pages/_app.js
+++ pages/_app.js
@@ -30,11 +30,12 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title> My page </title>
+        <title> fifness-admin </title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+          <link rel="icon" href="/logo.jpg" /> 
       </Head>
       {
       hiddenLayout || hiddenLayoutRegister ? (
